fa4be0ad19504410d4873f2b584f90c2030f54aa
sandi wicaksono <afganersml@gmail.com>
2025-08-27 14:30:17 +0700

    fixing bug2

--- routes/web.php
+++ routes/web.php
@@ -7,56 +7,41 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-
-Route::middleware('auth')->group(function () {
-    Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])
-        ->name('comments.store');
-});
-
-
-Route::get('/', function () {
-    return redirect()->route('threads.index');
-});
-
-// Dashboard (login required)
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
-
-Route::get('/register', function() {
-    return view('auth.register');
-});
-
-Route::get('/login', function() {
-    return view('auth.login');
-});
-
-// Threads (index & show publik; create/store butuh login)
-
-// --- Publik (BAIK untuk guest & user yang login) ---
-Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
-Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
-
-// --- Hanya butuh login untuk membuat thread ---
-Route::middleware('auth')->group(function () {
-    Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
-    Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
-});
-
+})->middleware(['auth'])->name('dashboard');
 
 // Menfess
 Route::get('/menfess', [MenfessController::class, 'index'])->name('menfess.index');
-Route::post('/menfess', [MenfessController::class, 'store'])->middleware('auth')->name('menfess.store');
+Route::post('/menfess', [MenfessController::class, 'store'])->name('menfess.store');
+
+// Threads
+Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
+Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
+Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
 
 
-// Komentar & Like (wajib login)
-// Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])
-//     ->middleware('auth')->name('comments.store');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->name('logout');
 
-// Route::post('/threads/{thread}/like', [LikeController::class, 'toggle'])
-//     ->middleware('auth')->name('threads.like');
+Route::middleware(['auth'])->group(function () {
 
-// Logout
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    // Route::get('/menfess', [MenfessController::class, 'index']);
+    // Route::post('/menfess', [MenfessController::class, 'store'])->name('menfess.store');
+
+});
 
 require __DIR__ . '/auth.php';
+
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
+//     Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
+
+
+Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::post('/threads/{thread}/like', [LikeController::class, 'toggle'])->name('threads.like');
+Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+
+// });
