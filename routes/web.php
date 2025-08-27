b591eb8a4c8242156c6fcdca208eb3f1be503ae8
sandi wicaksono <afganersml@gmail.com>
2025-08-27 11:03:39 +0700

    fixing bug2

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ use App\Http\Controllers\LikeController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 
-// Route::middleware('auth')->group(function () {
-//     Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])
-//         ->name('comments.store');
-// });
+Route::middleware('auth')->group(function () {
+    Route::post('/threads/{thread}/comments', [CommentController::class, 'store'])
+        ->name('comments.store');
+});
 
 
 Route::get('/', function () {
@@ -31,20 +31,18 @@ Route::get('/login', function() {
     return view('auth.login');
 });
 
+// Threads (index & show publik; create/store butuh login)
+
+Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
+Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
+Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
+
+
 
 // Menfess
 Route::get('/menfess', [MenfessController::class, 'index'])->name('menfess.index');
 Route::post('/menfess', [MenfessController::class, 'store'])->middleware('auth')->name('menfess.store');
-
-// Threads (index & show publik; create/store butuh login)
-Route::get('/threads', function () {
-    return redirect()->route('threads.index');
-});
-
-// Route::get('/threads', [ThreadController::class, 'index'])->name('threads.index');
-Route::get('/threads/create', [ThreadController::class, 'create'])->name('threads.create');
-Route::get('/threads/{thread}', [ThreadController::class, 'show'])->name('threads.show');
-Route::post('/threads', [ThreadController::class, 'store'])->name('threads.store');
 
 
 // Komentar & Like (wajib login)
